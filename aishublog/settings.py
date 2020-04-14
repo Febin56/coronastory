71a875edbd331575b99fafe5b182ed3398dc9eff
aiswaryap <aiswaryap333@gmail.com>
2020-04-15 03:38:27 +0530

    initial commit new again

--- aishublog/settings.py
+++ aishublog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +25,7 @@ SECRET_KEY = '@a6ju)39pv$y=1$=i^4xt#p4u=745cla$2l00if%^ioc%n37rl'
 #SECRET_KEY = os.environ.get('BLOG_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
